1a15a0221a30f81d13b01e35aaa64219665e8000
samba <gudurusamba@gmail.com>
2023-04-25 12:41:55 +0530

    terraform example

--- ex1/01-variables.tf
+++ ex1/01-variables.tf
@@ -1,7 +1,8 @@
 variable "string_name" {
 default = "hello world"
-
 }
-
+output "string_name" {
+  value = var.string_name
+}
 ## terraform destroy -auto-approve
 ## git pull ; terraform apply -auto-approve
