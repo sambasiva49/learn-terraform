de8df971566ed2b12dea088a4f3e280b9320833d
samba <gudurusamba@gmail.com>
2023-06-03 11:37:17 +0530

    terraform example

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "null" {
     count = 10
 }
+
+resource "null_resource" "pens" {
+  count = length(var.pens)
+}
+
+variable "pens" {
+  default = [  "red", "black", "blue"]
+}
