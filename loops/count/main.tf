37afae8d91cd065cc36e26ed336936e4ed134309
samba <gudurusamba@gmail.com>
2023-06-03 11:52:27 +0530

    terraform example

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -5,7 +5,11 @@ resource "null_resource" "null" {
 resource "null_resource" "pens" {
   count = length(var.pens)
 }
-
+ provisioner "loc_exc" {
+  command = "echo sambasiva pens${var.pens[count.index]}"
+}
 variable "pens" {
   default = [  "red", "black", "blue"]
 }
+
+// for future reference i am using pens insted of fruits
