cd95ce5287efef3bca365849ea9705fc55a3d278
samba <gudurusamba@gmail.com>
2023-06-15 16:17:16 +0530

    terraform example

--- loops/for-each/fil.tf
+++ loops/for-each/fil.tf
@@ -12,6 +12,6 @@ resource "null_resource" "items" {
   for_each = var.items
 
   provisioner "local-exec" {
-    command = "echo ${each.key} : ${each.value}"
+    command = "echo ${each.key} - ${each.value}"
   }
 }
