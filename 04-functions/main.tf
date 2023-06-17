4d259a9ed110e31206436a4f26d807d2d4825dcd
samba <gudurusamba@gmail.com>
2023-06-17 12:27:36 +0530

    terraform example

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -5,3 +5,15 @@ variable "sample" {
 output "sam" {
   value = upper(var.sample)
 }
+
+
+// this upper function is give upper values
+// element function gives the values which are given in the bracket that means if you
+// first value then it will be give first value
+ variable "sam1" {
+   default = ["abc" , "xyz"]
+ }
+
+output "sam2" {
+  value = element(var.sam1, 0)
+}
