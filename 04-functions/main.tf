c28d2b3cf117cfbdb706a26d8728057b47d75b0f
samba <gudurusamba@gmail.com>
2023-06-17 12:36:18 +0530

    terraform example

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -10,10 +10,25 @@ output "sam" {
 // this upper function is give upper values
 // element function gives the values which are given in the bracket that means if you
 // first value then it will be give first value
- variable "sam1" {
+
+variable "sam1" {
    default = ["abc" , "xyz"]
  }
 
 output "sam2" {
   value = element(var.sam1, 0)
+}
+
+// one more function s look up it is like if then function for example you take variable in that
+// variable you dont find the search vale it will give the dummy value
+
+variable "sam3" {
+  default = {
+          samba=100
+          siva =200
+  }
+}
+
+output "out" {
+  value = lookup(var.sam3 , "s" , "dummy")
 }
